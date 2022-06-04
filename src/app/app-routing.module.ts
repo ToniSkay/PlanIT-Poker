0346444bd948955d4ac53fde69fa872ed32a2123
Anton Chuevsky <a.chuevsky@itransition.com>
2022-06-05 00:13:17 +0300

    Add layout and base modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RoutePath} from "./shared/enums/route-path";
+import {LayoutComponent} from "./shared/layout/layout.component";
+import {RoomsComponent} from "./rooms/rooms.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: RoutePath.Root,
+    pathMatch: 'full',
+    redirectTo: RoutePath.Rooms,
+  },
+  {
+    path: RoutePath.Root,
+    component: LayoutComponent,
+    children: [
+      {
+        path: RoutePath.Rooms,
+        component: RoomsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    paramsInheritanceStrategy: 'always',
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
